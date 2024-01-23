14f9ad2422b96b93a20408b28d1367549753c749
John N. Norman <normanj777@gmail.com>
2024-01-23 11:51:40 -0500

    first commit

---
+++ lesson-2/src/main.ts
@@ -0,0 +1,19 @@
+let myName: string = "Dave";
+let meaningOfLife: number;
+let isLoading: boolean;
+let album: any;
+
+myName = "John ";
+meaningOfLife = 42;
+isLoading = true;
+album = "van Halen";
+album = 1984;
+
+const sum = (a: number, b: string) => {
+  return a + b;
+};
+
+let postId: string | number;
+let isActive: number | boolean;
+
+let re: RegExp = /\w+/g;
---
+++ lesson03/build/js/main.js
@@ -0,0 +1,16 @@
+"use strict";
+let myName = "Dave";
+let meaningOfLife;
+let isLoading;
+let album;
+myName = "John ";
+meaningOfLife = 42;
+isLoading = true;
+album = "van Halen";
+album = 1984;
+const sum = (a, b) => {
+    return a + b;
+};
+let postId;
+let isActive;
+let re = /\w+/g;
